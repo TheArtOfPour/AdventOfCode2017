a6bd9c6248053bf14ff6989f5b7e1b6c56694a2f
Justin Revelstoke <justin.revelstoke@gmail.com>
2017-12-01 14:00:53 -0800

    advent 1.51

--- main.go
+++ main.go
@@ -11,20 +11,16 @@ func main() {
 func advent(test string) (int, error) {
 	fmt.Printf("%s\n", test)
 	offset := len(test) / 2
-	//first := 0
 	opposite := 0
 	total := 0
-	for i, rune := range test {
+	for i, rune := range test[:offset] {
 		intRune := int(rune - '0')
 		circle := i + offset
-		if i+offset >= len(test) {
-			break
-		}
 		opposite = int(test[circle] - '0')
 		if opposite == intRune {
 			total += 2 * intRune
 		}
-		fmt.Printf("%d: %d %d\n", intRune, opposite, total)
+		//fmt.Printf("%d: %d %d\n", intRune, opposite, total)
 	}
 
 	return total, nil
